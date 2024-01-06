f2a9d83a19dbff9959dfdb92a78f5e70e322203f
lomi <151595407+lomiituu@users.noreply.github.com>
2024-01-06 12:56:46 +0800

    Products.java

--- Products.java
+++ Products.java
@@ -55,7 +55,7 @@ public class Products {
 			System.out.println("\t \t \t==================================================================================================================");
 			System.out.println("\t \t \t******************************************************************************************************************");
 			System.out.println("\t \t \t \t \t \t \t \t" + "       Supereme Grocery Store");
-			System.out.println("\t \t \t \t \t \t \t \t" + "      Balagtas Bulacan Bulacan");
+			System.out.println("\t \t \t \t \t \t \t \t" + "      Balagtas, Bulacan Bulacan");
 			System.out.println("\t \t \t \t \t \t \t \t" + "      Contact No : 09564551233");
 			System.out.println("\t \t \t==================================================================================================================");
 			System.out.println("\t \t \t******************************************************************************************************************");
@@ -64,28 +64,28 @@ public class Products {
 	}
 	
 	public static void login() {
-		System.out.print("\t \t \t \t" + "Enter a username: ");
+		System.out.print("\t \t \t \t" + "Enter your username: ");
 		String usern = scn.next();
-		System.out.print("\t \t \t \t" + "Enter a password: ");
+		System.out.print("\t \t \t \t" + "Enter your password: ");
 		String pass = scn.next();
 
 		if(usern.equals("admin") && pass.equals("admin123")) {
-			System.out.println("\t \t \t \t" + "Account successfully logged in.");
+			System.out.println("\t \t \t \t" + "Your account has been successfully logged in.");
 			System.out.println("\t \t \t==================================================================================================================");
 		}
 		else {
-			System.out.println("\t \t \t \t" + "Account doesn't exist. ");
+			System.out.println("\t \t \t \t" + "Account doesn't exist, Please try logging in again or register a new one. ");
 			Products p = new Products();
 			p.main(null);
 		}
 	}
 	
 	  public static void displayMenu() {
-	        System.out.println("\t \t \t \t" + "==================================");
-	        System.out.println("\t \t \t \t" + "| 1. Buy product                 |");
-	        System.out.println("\t \t \t \t" + "| 2. Cancel product              |");
-	        System.out.println("\t \t \t \t" + "| 3. Exit                        |");
-	        System.out.println("\t \t \t \t" + "==================================");
+	        System.out.println("\t \t \t \t" + "------------------------------------");
+	        System.out.println("\t \t \t \t" + "|   1. Buy product                 |");
+	        System.out.println("\t \t \t \t" + "|   2. Cancel product              |");
+	        System.out.println("\t \t \t \t" + "|   3. Exit                        |");
+	        System.out.println("\t \t \t \t" + "------------------------------------");
 	        System.out.print("\t \t \t \t" + "Enter your choice: ");
 	   }
 	
