c84e2bd6206d8c5675fbd3880a828e71a94a7a13
Tristanityyyyy <145412794+Tristanityyyyy@users.noreply.github.com>
2024-01-08 17:05:49 +0800

    Update Products.java

--- Products.java
+++ Products.java
@@ -1,5 +1,4 @@
 package package1;
-
 import java.util.*;
 
 public class Products {
@@ -7,7 +6,8 @@ public class Products {
 	static Scanner scn = new Scanner(System.in);
 	static Store s = new Store();
 	static boolean status = false;
-	public static void main(String[] args) {
+	
+	public static void main(String[] args)throws Exception {
 		
 		while(!status) {
 			menu();
@@ -22,30 +22,25 @@ public class Products {
 					System.out.println("Invalid input. Try again.");
 			}
 			
-			
+            displayMenu();
 	        while (!status) {
-	            displayMenu();
 	            int choice = scn.nextInt();
 	            
 	            switch (choice) {
 	                
 	                case 1:
-	                	Store.buyProd();
+	                	System.out.println("\t \t \t \t" + "PLEASE TYPE [X] WHEN YOU ARE DONE.");
+	                	s.buyProd();
 	                	break;
-	                    
+	
 	                case 2:
-	                    cancelProduct();
-	                    break;
-	                    
-	                
-	                case 3:
 	                    status = true;
 	                    break;
 	                    
 	                default:
 	                    System.out.println("Invalid choice");
 	            }
-		
+	            break;
 	        }
 	    }
 		
@@ -56,48 +51,41 @@ public class Products {
 			System.out.println("\t \t \t==================================================================================================================");
 			System.out.println("\t \t \t******************************************************************************************************************");
 			System.out.println("\t \t \t \t \t \t \t \t" + "       Supereme Grocery Store");
-			System.out.println("\t \t \t \t \t \t \t \t" + "      Balagtas, Bulacan Bulacan");
+			System.out.println("\t \t \t \t \t \t \t \t" + "      Balagtas Bulacan Bulacan");
 			System.out.println("\t \t \t \t \t \t \t \t" + "      Contact No : 09564551233");
 			System.out.println("\t \t \t==================================================================================================================");
 			System.out.println("\t \t \t******************************************************************************************************************");
 			System.out.println("\t \t \t \t" + "Please log-in your account or sign up.");
 			System.out.print("\t \t \t \t" + "Press 'L' to log-in. ");
 	}
-	
+	static Products p = new Products();
 	public static void login() {
-		System.out.print("\t \t \t \t" + "Enter your username: ");
-		String usern = scn.next();
-		System.out.print("\t \t \t \t" + "Enter your password: ");
-		String pass = scn.next();
-
-		if(usern.equals("admin") && pass.equals("admin123")) {
-			System.out.println("\t \t \t \t" + "Your account has been successfully logged in.");
-			System.out.println("\t \t \t==================================================================================================================");
-		}
-		else {
-			System.out.println("\t \t \t \t" + "Account doesn't exist, Please try logging in again or register a new one. ");
-			Products p = new Products();
-			Products.main(null);
-		}
+		
+		
+			System.out.print("\t \t \t \t" + "Enter a username: ");
+			String usern = scn.next();
+			System.out.print("\t \t \t \t" + "Enter a password: ");
+			String pass = scn.next();
+		
+			if(usern.equals("admin") && pass.equals("admin123")) {
+				System.out.println("\t \t \t \t" + "Account successfully logged in.");
+				System.out.println("\t \t \t==================================================================================================================");
+			}
+			else {
+				System.out.println("\t \t \t \t" + "Account doesn't exist. ");
+			}
 	}
 	
 	  public static void displayMenu() {
-	        System.out.println("\t \t \t \t" + "------------------------------------");
-	        System.out.println("\t \t \t \t" + "|   1. Buy product                 |");
-	        System.out.println("\t \t \t \t" + "|   2. Cancel product              |");
-	        System.out.println("\t \t \t \t" + "|   3. Exit                        |");
-	        System.out.println("\t \t \t \t" + "------------------------------------");
+	        System.out.println("\t \t \t \t" + "==================================");
+	        System.out.println("\t \t \t \t" + "| 1. Buy product                 |");
+	        System.out.println("\t \t \t \t" + "| 2. Exit                        |");
+	        System.out.println("\t \t \t \t" + "==================================");
 	        System.out.print("\t \t \t \t" + "Enter your choice: ");
 	   }
+	  
 	
-	  
-	  
-	  public static void cancelProduct() {
-		  System.out.println("Your transaction has been canceled.");
-                  System.out.println("Would you like to buy products or exit?");
-		  
-	  }
-	 
+	
 }
 
 	
