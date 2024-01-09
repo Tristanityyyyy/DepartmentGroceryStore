c4c7b938eaa6bb1a202e6b2e108f5d60222fd6c3
Tristanityyyyy <145412794+Tristanityyyyy@users.noreply.github.com>
2024-01-09 12:09:09 +0800

    Update Products.java

--- Products.java
+++ Products.java
@@ -1,236 +1,107 @@
 package package1;
-
 import java.util.*;
 
-public class Store{
+public class Products {
+	
 	static Scanner scn = new Scanner(System.in);
+	static Store s = new Store();
 	
-	static Queue <Double> resultQuant = new LinkedList();
-	static Queue <String> resultName = new LinkedList();
-	static Queue <Double>resultPrice = new LinkedList();
+	static boolean status = false;
 	
-	static LinkedHashMap <String,String> prodHash = new LinkedHashMap<>();
-	static ArrayList <Integer> quantity = new ArrayList();
-	static ArrayList <Double>price = new ArrayList();
-
-	static Queue <String> orderedProd = new LinkedList();
-	static Queue <Double> orderedQuantity = new LinkedList();
-	static Queue <Double>orderedPrice = new LinkedList();
-	static Computation c = new Computation();
-	
-	static Queue <String> cancProd = new LinkedList();
-	
-	static void products() {
-		prodHash.put("00001","Dozen of eggs     ");
-		prodHash.put("00002","50 kg Rice        ");
-		prodHash.put("00003","50 kg BrownRice   ");
-		prodHash.put("00004","Cooking Oil       ");
-		prodHash.put("00005","Vinegar / Suka    ");
-		prodHash.put("00006","Bottled Water     ");
-		prodHash.put("00007","Pack of Coffee    ");
-		prodHash.put("00008", "Milk Shake       ");
-		prodHash.put("00009", "Ice Cream        ");
-		prodHash.put("00010", "Pack of Sugar    ");
-		prodHash.put("00011", "Soda Drink       ");
-		prodHash.put("00012", "Corned Beef      ");
-		prodHash.put("00013", "All purpose Cream");
-		
-	}
-	
-	static String prodId;
-	static double qt;
-	
-	
-	static double cash;
-	
-	public static void buyProd(){
-		try {
-			Collections.addAll(quantity, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100);
-			Collections.addAll(price,230.00, 2000.00, 2500.00, 130.00, 250.00, 25.00, 180.00 , 100.00, 300.00, 270.00, 85.00, 70.00, 150.00);
-			
-			products();		
-			System.out.println("\t \t \t==================================================================================================================");
-			System.out.println("\t \t \t \t" + "Code \t " + "Product \t \t \t \t \t \t \t" + "Quantity    " + "Price \n");
-			
-			
-			
-			int incre = 0;
-			for(Map.Entry <String , String> e : prodHash.entrySet()) {
-				for (int i = 0; i < 1; i++) {
-					
-					System.out.print("\t \t \t \t" + e.getKey());
-					System.out.print("\t" + e.getValue());
-					
-					System.out.print("\t \t \t \t \t \t" + quantity.get(incre));
-					System.out.println("         "  + price.get(incre));
-					incre++;
-				}
-			}
-			
-			
-			System.out.println("\t \t \t==================================================================================================================");
-			
-			boolean a = true;
-			
-			while(a) {
-				System.out.println("\t \t \t \t" + "PLEASE TYPE [X] WHEN YOU ARE DONE.");
-				System.out.print("\t \t \t \t" + "Enter product id: \t \t");
-				prodId = scn.next();
-				
-				if(prodId.equalsIgnoreCase("X") ) {
-					break;
-				}
-				
-				System.out.print("\t \t \t \t" + "Enter product quantity: \t");
-				qt = scn.nextDouble();
-
-				System.out.println();
-				if(!prodId.equalsIgnoreCase("X") || qt == 0) {
-						resultName.offer(prodHash.get(prodId));
-						resultQuant.offer(qt);
-				}
-			}
-	
-			System.out.println("\t \t \t==================================================================================================================");
-			System.out.println("\t \t \t \t|Product| \t \t \t \t \t|Quantity|         |Price|");
-			System.out.println("\t \t \t==================================================================================================================");
-			
-			while(!resultName.isEmpty()) {
-				
-				System.out.print("\t \t \t \t" + resultName.peek());
-				System.out.print("\t \t \t \t" + resultQuant.peek());
-				
-				orderedProd.offer(resultName.poll());
-				orderedQuantity.offer(resultQuant.poll()); //SAVEDDDD NAAA QT
-
-				int incre2 = 0;
-	
-				for(Map.Entry <String , String> e : prodHash.entrySet()) {
-					if(e.getValue().equals(orderedProd.peek())) { // keme.peek
-						System.out.println("\t \t   "+ price.get(incre2));
-						orderedPrice.offer(price.get(incre2)); ////SAVEDDDD NAAAA PRICESSS
-						incre2++;
-						cancProd.offer(orderedProd.poll());
-					}
-					else {
-						incre2++;
-					}
-				}	    
-			}
-			c.computationProcess();
-			displayPayment();
+	public static void main(String[] args) {
+		menu();
+		Computation c = new Computation();
+		if(c.pricewCharge == 0) {
+			login();
 		}
-			catch(InputMismatchException e) {
-				System.out.println("\t \t \t \tPlease try again");
-				Products.main((null));
-			}
-	}
+		while(!status) {
+			
+            displayMenu();
+	        while (!status) {
+	            int choice = scn.nextInt();
+	            
+	            switch (choice) {
+	                
+	                case 1:
+	                	System.out.println("\t \t \t \t" + "PLEASE TYPE [X] WHEN YOU ARE DONE.");
+	                	s.buyProd();
+	                	break;
 	
-	public static void displayPayment() {
-		Scanner scn = new Scanner(System.in);
-		System.out.println("\t \t \t------------------------------------------------------------------------------------------------------------------");
-		
-	  	System.out.println("\t \t \t \t \t \t \t \t \t \tTAXABLE TOTAL    : " + c.totalPrice);
-	  	System.out.println("\t \t \t \t \t \t \t \t \t \tVat(12%)         : " + c.vatPrice + "\n");
-	  	System.out.println("\t \t \t \t \t \t \t \t \t \tTOTAL            : " + c.pricewCharge);
-
-		System.out.println("\t \t \t------------------------------------------------------------------------------------------------------------------\n");
-		System.out.println("\t \t \t \t" + "==================================");
-	    System.out.println("\t \t \t \t" + "| 1. Pay                         |");
-	    System.out.println("\t \t \t \t" + "| 2. Cancel product              |");
-	    System.out.println("\t \t \t \t" + "| 3. Exit                        |");
-	    System.out.println("\t \t \t \t" + "| 4. Menu                        |");
-	    System.out.println("\t \t \t \t" + "==================================");
-	    System.out.print("\t \t \t \t" + "Enter your choice: ");
-	    int choice = scn.nextInt();
-	    switch(choice) {
-	    
-	    case 1:
-		  	System.out.print("\t \t \t \t \t \t \t \t \t \t \t AMOUNT  : ");
-			cash = scn.nextDouble();
-			double change = cash - c.pricewCharge;
-			Math.round(change);
-		  	System.out.println("\t \t \t \t \t \t \t \t \t \t \t CASH    : " + cash);
-		  	System.out.println("\t \t \t \t \t \t \t \t \t \t \t CHANGE  : " + change);
-	    	cancProd.remove(prodHash.values());
-	    	cancProd2.remove(prodHash.values());
-	    	cancProd2.remove(prodHash.values());
-	    	cancProd3.remove(prodHash.values());
-	    	cancProd3.remove(prodHash.values());
-
-			break;
-	    case 2:
-	    	cancelProduct();
-	    case 3:
-	    	break;
-	    case 4:
-	    	Products.main(null);
+	                case 2:
+	                    status = true;
+	                    break;
+	                case 3:
+	            	    Admin.display();;
+	                default:
+	                    System.out.println("Invalid choice");
+	            }
+	            break;
+	        }
 	    }
-	    
-	}
-	
-
-	static Queue <Double> quant = new LinkedList();
-	static Queue <String> cancProd2 = new LinkedList();
-	static Queue <Double> quant2 = new LinkedList(); 
-	static Queue <String> cancProd3 = new LinkedList();
-
-	public static void cancelProduct() {
-		System.out.println("\t \t \t \t" + "PLEASE ENTER [X] WHEN YOU ARE DONE");
-		boolean a = true;
-		while(a) {
-			System.out.print("\t \t \t \t" + "Enter product id: \t \t");
-			String prodId2 = scn.next();
-			int incre4 = 0;
-			for(Map.Entry <String , String> e : prodHash.entrySet()) {
-				if(prodHash.containsKey(prodId2)) {
-					quant.offer(price.get(incre4)); //removing of price na di kailangan
-					incre4++;
-				}
-				else {
-					incre4++;
-				}
-			}
-			if(prodHash.containsKey(prodId2)) {
-				cancProd.remove(prodHash.get(prodId2));
-			}
-			else if(prodId2.equalsIgnoreCase("X")) {
-				while(a) {
-					cancProd.remove(prodHash.values());
-					break;
-				}
-				break;
-			}
-		}
-		System.out.println("\t \t \t==================================================================================================================");
-		System.out.println("\t \t \t \t|Product| \t \t \t \t \t|Quantity|         |Price|");
-		System.out.println("\t \t \t==================================================================================================================");
 		
-				while(!cancProd.isEmpty()) {
-				System.out.print("\t \t \t \t" + cancProd.peek());
-				System.out.print("\t \t \t \t" + quant.peek());
+	}
+
+	public static void menu() {
+			System.out.println("\t \t \t==================================================================================================================");
+			System.out.println("\t \t \t \t \t \t \t      _       __     __                        \r\n"
+					+ "\t \t \t \t \t \t \t     | |     / /__  / /________  ____ ___  ___ \r\n"
+					+ "\t \t \t \t \t \t \t     | | /| / / _ \\/ / ___/ __ \\/ __ `__ \\/ _ \\\r\n"
+					+ "\t \t \t \t \t \t \t     | |/ |/ /  __/ / /__/ /_/ / / / / / /  __/\r\n"
+					+ "\t \t \t \t \t \t \t     |__/|__/\\___/_/\\___/\\____/_/ /_/ /_/\\___/ \r\n"
+					+ "                                          ");
+			System.out.println("\t \t \t==================================================================================================================");
+
+		                                           
+			System.out.println("\t \t \t==================================================================================================================");
+			System.out.println("\t \t \t******************************************************************************************************************");
+			System.out.println("\t \t \t \t \t \t \t \t" + "       Supereme Grocery Store");
+			System.out.println("\t \t \t \t \t \t \t \t" + "      Balagtas Bulacan Bulacan");
+			System.out.println("\t \t \t \t \t \t \t \t" + "      Contact No : 09564551233");
+			System.out.println("\t \t \t==================================================================================================================");
+			System.out.println("\t \t \t******************************************************************************************************************");
+			
+	}
+	static Products p = new Products();
+	public static void login() {	
+			System.out.println("\t \t \t \t" + "Please log-in your account or sign up.");
+			System.out.print("\t \t \t \t" + "Press 'L' to log-in. ");
+			char sign = scn.next().toUpperCase().charAt(0);
+			System.out.println(" ");
+			
+			switch(sign) {
+				case 'L':
+					System.out.print("\t \t \t \t" + "Enter a username: ");
+					String usern = scn.next();
+					System.out.print("\t \t \t \t" + "Enter a password: ");
+					String pass = scn.next();
 				
-				cancProd2.offer(cancProd.poll());
-				quant2.offer(quant.poll()); 
-				int incre3 = 0;
-				
-				for(Map.Entry <String , String> e : prodHash.entrySet()) {
-					if(e.getValue().equals(cancProd2.peek())) {
-						System.out.println("\t \t   "+ price.get(incre3));
-						orderedPrice.offer(price.get(incre3));
-						incre3++;
-						cancProd3.offer(cancProd2.poll());
+					if(usern.equals("admin") && pass.equals("admin123")) {
+						System.out.println("\t \t \t \t" + "Account successfully logged in.");
+						System.out.println("\t \t \t==================================================================================================================");
 					}
 					else {
-						incre3++;
+						System.out.println("\t \t \t \t" + "Account doesn't exist. ");
+						Products.main(null);
 					}
-				}
-			}
-				if(cancProd.isEmpty()) {
+				break;
+				default:
+					System.out.println("Invalid input. Try again.");
 					Products.main(null);
-				}
-					c.computationProcessCancel();
-					displayPayment();
+			}
+			
 	}
+	
+	  public static void displayMenu() {
+	        System.out.println("\t \t \t \t" + "==================================");
+	        System.out.println("\t \t \t \t" + "| 1. Buy product                 |");
+	        System.out.println("\t \t \t \t" + "| 2. Exit                        |");
+		System.out.println("\t \t \t \t" + "| 3. Admin                       |");
+	        System.out.println("\t \t \t \t" + "==================================");
+	        System.out.print("\t \t \t \t" + "Enter your choice: ");
+	   }
+	  
+	
+	
 }
 
+	
