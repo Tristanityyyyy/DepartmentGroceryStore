c80eeaf6f98cf06a643ffec7504cc7b0b8ccc4e0
Tristanityyyyy <145412794+Tristanityyyyy@users.noreply.github.com>
2024-01-19 09:26:03 +0800

    Update Products.java

--- Products.java
+++ Products.java
@@ -1,27 +1,40 @@
-
-package products;
+package package1;
 import java.util.*;
 
 public class Products {
+	static Queue <String> usernames = new LinkedList();
+	static Queue <String> passwords = new LinkedList();
+	static Scanner scn = new Scanner(System.in);
+	static Store s = new Store();
 	
-	public static void main(String[] args){
-            Scanner scn = new Scanner(System.in);
-            Store st  = new Store();
-            boolean status = false;
-		
+	static boolean status = false;
+	
+	public static void main(String[] args) {
+		run();
+	}
+	
+	static void run() {
 		menu();
+		Computation c = new Computation();
+		
+		login();
+	
+		
+		while(!status) {
+			
+            displayMenu();
 	        while (!status) {
 	            int choice = scn.nextInt();
 	            
 	            switch (choice) {
 	                
 	                case 1:
-	                    st.buyProd();
-                            buyAgain();
-	                    break;
-	               
+	                	s.buyProd();
+	                	break;
+	
 	                case 2:
-	            	    System.out.close();
+	                    status = true;
+	                    break;
 	                default:
 	                    System.out.println("Invalid choice");
 	            }
@@ -29,95 +42,78 @@ public class Products {
 	        }
 	    }
 		
-	
-                public static void menu() {
-                    Scanner scn = new Scanner(System.in);
-                    System.out.println("\t \t \t==================================================================================================================");
-                    System.out.println("""
-                                       \t \t \t \t \t \t \t      _       __     __                        \r
-                                       \t \t \t \t \t \t \t     | |     / /__  / /________  ____ ___  ___ \r
-                                       \t \t \t \t \t \t \t     | | /| / / _ \\/ / ___/ __ \\/ __ `__ \\/ _ \\\r
-                                       \t \t \t \t \t \t \t     | |/ |/ /  __/ / /__/ /_/ / / / / / /  __/\r
-                                       \t \t \t \t \t \t \t     |__/|__/\\___/_/\\___/\\____/_/ /_/ /_/\\___/ \r
-                                                                                 """);
-                    System.out.println("\t \t \t==================================================================================================================");
-
-                    System.out.println("\t \t \t==================================================================================================================");
-                    System.out.println("\t \t \t******************************************************************************************************************");
-                    System.out.println("\t \t \t \t \t \t \t \t" + "       Supreme Grocery Store");
-                    System.out.println("\t \t \t \t \t \t \t \t" + "      Balagtas Bulacan Bulacan");
-                    System.out.println("\t \t \t \t \t \t \t \t" + "      Contact No : 09564551233");
-                    System.out.println("\t \t \t==================================================================================================================");
-                    System.out.println("\t \t \t******************************************************************************************************************");
-                    System.out.println("\t \t \t \t" + "Please log-in your account or sign up.");
-                    System.out.println("\t \t \t \t" + "1. Log in as Customer");
-                    System.out.println("\t \t \t \t" + "2. Log in as Admin");
-                    System.out.print("\t \t \t \t" + "Enter your choice: ");
-
-                    int choice = scn.nextInt();
-                    switch (choice) {
-                        case 1 -> login("customer");
-                        case 2 -> Admin.display();
-                        default -> {
-                            System.out.println("\t \t \t \t" + "Invalid input. Try again.");
-                            
-                        } 
-                       
-                    }
-                  
-                } 
-	public static void login(String customer) {
-		  Scanner scn = new Scanner(System.in);
-		        System.out.println("");
-			System.out.print("\t \t \t \t" + "Enter a username: ");
-			String usern = scn.next();
-			System.out.print("\t \t \t \t" + "Enter a password: ");
-			String pass = scn.next();
 		
-			if(!usern.equals("customer") && !pass.equals("customer123")) {
-                            System.out.println("\t \t \t \t" + "Account doesn't exist. ");
-                        }
-			else {
-                            System.out.println("\t \t \t \t" + "Account successfully logged in.");
-                            System.out.println("\t \t \t=================================================================================================================="); 
-                            displayMenu();
-                        } 
-                      
+	}
+	public static void menu() {
+			System.out.println("\t \t \t==================================================================================================================");
+			System.out.println("\t \t \t \t \t \t \t      _       __     __                        \r\n"
+					+ "\t \t \t \t \t \t \t     | |     / /__  / /________  ____ ___  ___ \r\n"
+					+ "\t \t \t \t \t \t \t     | | /| / / _ \\/ / ___/ __ \\/ __ `__ \\/ _ \\\r\n"
+					+ "\t \t \t \t \t \t \t     | |/ |/ /  __/ / /__/ /_/ / / / / / /  __/\r\n"
+					+ "\t \t \t \t \t \t \t     |__/|__/\\___/_/\\___/\\____/_/ /_/ /_/\\___/ \r\n"
+					+ "                                          ");
+			System.out.println("\t \t \t==================================================================================================================");
+
+		                                           
+			System.out.println("\t \t \t==================================================================================================================");
+			System.out.println("\t \t \t******************************************************************************************************************");
+			System.out.println("\t \t \t \t \t \t \t \t" + "       Supreme Grocery Store");
+			System.out.println("\t \t \t \t \t \t \t \t" + "      Balagtas Bulacan Bulacan");
+			System.out.println("\t \t \t \t \t \t \t \t" + "      Contact No : 09564551233");
+			System.out.println("\t \t \t==================================================================================================================");
+			System.out.println("\t \t \t******************************************************************************************************************");
+			
+	}
+	static Products p = new Products();
+	public static void login() {	
+			System.out.println("\t \t \t \t" + "Please log-in your account or sign up.");
+			System.out.print("\t \t \t \t" + "Press 'L' to log-in. ");
+			char sign = scn.next().toUpperCase().charAt(0);
+			System.out.println(" ");
+			
+			switch(sign) {
+				case 'L':
+					System.out.print("\t \t \t \t" + "Enter a username: ");
+					String usern = scn.next();
+					System.out.print("\t \t \t \t" + "Enter a password: ");
+					String pass = scn.next();
+				
+					if(usern.equals("cashier") && pass.equals("cashier123")) {
+						System.out.println("\t \t \t \t" + "Account successfully logged in.");
+						System.out.println("\t \t \t==================================================================================================================");
+					}
+					else if(usern.equals("admin") && pass.equals("admin123")) {
+						System.out.println("\t \t \t \t" + "Account successfully logged in.");
+						System.out.println("\t \t \t==================================================================================================================");
+						Admin.invent();
+						//ADMIN CALLING
+					}
+					else if(usernames.contains(usern) && passwords.contains(pass)) {
+						System.out.println("\t \t \t \t" + "Account successfully logged in.");
+						System.out.println("\t \t \t==================================================================================================================");
+					}
+					else {
+						System.out.println("\t \t \t \t" + "Account doesn't exist. ");
+						Products.main(null);
+					}
+				break;
+				default:
+					System.out.println("Invalid input. Try again.");
+					Products.main(null);
+			}
+			
 	}
 	
-	public static void displayMenu() {
+	  public static void displayMenu() {
 	        System.out.println("\t \t \t \t" + "==================================");
 	        System.out.println("\t \t \t \t" + "| 1. Buy product                 |");
 	        System.out.println("\t \t \t \t" + "| 2. Exit                        |");
 	        System.out.println("\t \t \t \t" + "==================================");
 	        System.out.print("\t \t \t \t" + "Enter your choice: ");
 	   }
-            public static void buyAgain(){
-                Scanner scn = new Scanner(System.in);
-                    char buyAgain;
-                    do {
-                        System.out.print(" \t \t \t \t " + "Do you want to buy again? (yes for y and no for n): ");
-                        buyAgain = scn.nextLine().charAt(0);
-            
-                    switch (Character.toLowerCase(buyAgain)) {
-                        case 'y':
-                            displayMenu(); 
-                            int choice = scn.nextInt();
-                            Store.buyProd();
-                            buyAgain();
-                            break;
-                        case 'n':
-                            System.out.println(" \t \t \t \t " + "Thank you for shopping!");
-                            break;
-                        default:
-                            System.out.println(" \t \t \t \t " + "Invalid input. Please enter 'y' or 'n'.");
-                            break;
-                    }
-
-                    } while (!(Character.toLowerCase(buyAgain) == 'y' || Character.toLowerCase(buyAgain) == 'n'));
-
-
-            }
-    }
 	  
+	
+	
+}
 
+	
